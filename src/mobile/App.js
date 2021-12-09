b1b5e608e96d13aac9506920b6c6b6413cc41917
Filipe Lima <filipe.lima@outlook.ie>
2021-12-09 12:48:06 +0000

    Ädded keep for backend

--- src/mobile/App.js
+++ src/mobile/App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, Button, TextInput, ImageBackground } from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = useState(null)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/pea.png')} style={styles.backgroundImage}>
+        <Image source={require('./assets/silvia.png')} style={styles.image} />
+      </ImageBackground>
+      <View style={styles.signUpForm}>
+        <View>
+          <Text style={styles.title}>I'm in the</Text>
+          <Text style={styles.titleSub}>office.</Text>
+        </View>
+        <TextInput placeholder='Email' value={email} onChangeText={setEmail} keyboardType='email-address' style={styles.email} />
+        <Button title='Sign Up' />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  signUpForm: {
+    flex: 1,
+    marginLeft: 32,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly'
+  },
+  title: {
+    fontSize: 32,
+  },
+  titleSub: {
+    fontSize: 32,
+    fontWeight: 'bold'
+  }
 });
