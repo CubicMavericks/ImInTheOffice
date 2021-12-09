0ebf643251a1be9a9eb009193b691e4d4b99efa1
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2021-12-09 22:24:39 +0000

    Create officeService.js

---
+++ src/web/src/services/officeService.js
@@ -0,0 +1,30 @@
+export default class OfficeService{
+    async checkIn(userId){
+       return request(userId, "checkin")
+    }
+    async checkOut(userId){
+        return request(userId, "checkout")
+    }
+}
+const request = async (userId, resource) => {
+    const requestOptions = {
+        crossDomain:true,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    };
+
+    try {
+        debugger;
+        var response = await fetch('https://localhost:7128/office/' + resource + "/" + userId, requestOptions)
+        const data = await response.json();
+    
+        if(!response.ok){
+                const error = (data && data.message) || response.stastus;
+                return error;
+        }
+        return data;
+    } catch (error) {
+        debugger;
+    }
+    
+}
