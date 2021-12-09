18dfa1f970433e85965df3657dcc2ca115373b59
Filipe Lima <filipe.lima@outlook.ie>
2021-12-09 22:07:46 +0000

    Small refactoring around auth

--- src/web/src/services/authService.js
+++ src/web/src/services/authService.js
@@ -1,32 +1,32 @@
-
 export default class AuthService {
-    signIn(email, callback) {
-        const requestOptions = {
-            crossDomain:true,
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email })
-        };
-        fetch('https://localhost:7128/user/signin', requestOptions)
-       // .then(response => response.json())
-        .then(async response => {
-            const data = await response.json();
-            // check for error response
-            if (!response.ok) {
-                // get error message from body or default to response status
-                const error = (data && data.message) || response.stastus;
-                return Promise.reject(error);
-            }
-            if(callback){
-                callback(data);
-            }
-        })
-        .catch(error => {
-            console.error('There was an error!', error);
-        });
-    }
+  signIn(email, callback) {
+    
+    const requestOptions = {
+      crossDomain: true,
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email }),
+    };
 
-    signOut(callback) {
-        return callback(true);
-    }
+    fetch("https://localhost:7128/user/signin", requestOptions)
+      .then(async (response) => {
+        const data = await response.json();
+
+        if (!response.ok) {
+          const error = (data && data.message) || response.status;
+          return Promise.reject(error);
+        }
+
+        if (callback) {
+          callback(data);
+        }
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  }
+
+  signOut(callback) {
+    return callback(true);
+  }
 }
